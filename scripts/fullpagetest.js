5f904dd67f632868a21ea5858cbc5ae59e9f9425
Dmitri Golota <dmitri.golota@gmail.com>
2021-03-11 17:48:36 -0800

    renamed stylsheet, added popupoverlay functionality

--- scripts/fullpagetest.js
+++ scripts/fullpagetest.js
@@ -12,3 +12,10 @@ myAudio.onpause = function() {
   isPlaying = false;
 };
 
+function open_litebox(link) {
+  var overlay_black = 'position:absolute;top:0%;left:0%;width:100%;height:100%;background:black;z-index:1001;opacity:0.5;';
+  var overlay_white = 'position:absolute;top:50%;left:50%;background-color:white;z-index:1002;overflow:auto;width:400px;height:400px;margin-left:-200px;margin-top:-200px';
+  $('body').append('<div style="' + overlay_black + '"></div>');
+  var overlay_white = 'position:absolute;top:25%;left:25%;padding:5px;background-color:white;z-index:1002;overflow:auto;';
+  $('body').append('<div style="' + overlay_white + '"><p>heeeyyy</p></div>');
+}
