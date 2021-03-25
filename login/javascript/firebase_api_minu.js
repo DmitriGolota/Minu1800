ad82bde4cf1e0373988016a21db7e20b67af7a6b
CALEB VERMA <halasmish@gmail.com>
2021-03-25 14:52:50 -0700

    link login to landing page

--- login/javascript/firebase_api_minu.js
+++ login/javascript/firebase_api_minu.js
@@ -29,7 +29,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: "insert url to landing page",
+    signInSuccessUrl: "http://127.0.0.1:5500/index.html",
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
