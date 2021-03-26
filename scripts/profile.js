4638a585d987c2c1ba3cef2b51ae4f33bae736fd
Dmitri Golota <dmitri.golota@gmail.com>
2021-03-26 10:26:35 -0700

    added firebase read function, needs debugging

--- scripts/profile.js
+++ scripts/profile.js
@@ -25,3 +25,21 @@ function editBusiness() {
     prompt("Please enter your business name");
 }
 
+// write name to name field from user db
+function writeName(){
+    firebase.auth().onAuthStateChanged(function(somebody){
+        if(somebody){
+            console.log(somebody.uid);
+            db.collection("users")
+            .doc(somebody.uid)
+            .get()
+            .then(function(doc){
+                console.log(doc.data().name);
+                var n = doc.data().name;
+
+                $("#name-from-db").text(n)
+            })
+        }
+    })
+}
+writeName();
