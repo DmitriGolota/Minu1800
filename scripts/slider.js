93fd565b6f9a89f1e47df294f3a1bf226675be80
CALEB VERMA <halasmish@gmail.com>
2021-04-05 00:01:24 -0700

    add dark and light theme to index.html

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,12 +4,20 @@ slider.addEventListener('change', function() {
     if (this.checked) {
         let mainBackground = document.getElementsByClassName("main-background");
         for(i = 0; i < mainBackground.length; i++) {
-            mainBackground[i].style.backgroundColor = 'blue';
+            mainBackground[i].style.backgroundColor = 'white';
+        }
+        let changeableText = document.getElementsByClassName("changeable-text");
+        for(i = 0; i < changeableText.length; i++) {
+            changeableText[i].style.color = 'black';
         }
     } else {
         let mainBackground = document.getElementsByClassName("main-background");
         for(i = 0; i < mainBackground.length; i++) {
-            mainBackground[i].style.backgroundColor = 'red';
+            mainBackground[i].style.backgroundColor = '#26272b';
+        }
+        let changeableText = document.getElementsByClassName("changeable-text");
+        for(i = 0; i < changeableText.length; i++) {
+            changeableText[i].style.color = 'white';
         }
     }
   });
