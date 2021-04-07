13c99535231663890369c621cd21b31ca2bca098
Dmitri Golota <dmitri.golota@gmail.com>
2021-04-07 12:18:36 -0700

    Remove Modalss

--- scripts/index.js
+++ scripts/index.js
@@ -13,37 +13,8 @@ myAudio.onpause = function() {
   isPlaying = false;
 };
 
-/// Modal ///
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 /// profile button ///
-
 
 document.getElementById("profile").addEventListener("click", function(event){
       window.location = "./login.html"
 });
-
-
-
-
-/// link on button click triggers
-//document.getElementById('template').addEventListener('click', function (event) {};
-//document.getElementById('guidedworkflow').addEventListener('click', function (event) {};
-//document.getElementById('freestyle').addEventListener('click', function (event) {};
