edc6b0e6bb837175eaa98a3073928f32e8fee8f9
Dmitri Golota <dmitri.golota@gmail.com>
2021-04-09 10:08:11 -0700

    added template save demo working functions

---
+++ scripts/menutemplate.js
@@ -0,0 +1,26 @@
+
+// Define the function 
+// to screenshot the div
+function takeshot() {
+    let div =
+        document.getElementById('photo');
+
+    // Use the html2canvas
+    // function to take a screenshot
+    // and append it
+    // to the output div
+    html2canvas(div).then(
+        function (canvas) {
+
+            document.getElementById('output').appendChild(canvas);
+
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF();
+
+            pdf.addImage(imgData, 'JPEG', 0, 0);
+            pdf.save("menu.pdf");
+            console.log("successful functions execution")
+
+        })
+}
+
