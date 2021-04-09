12411624128e1cd290dfc51224eedb10a16449dd
Dmitri Golota <dmitri.golota@gmail.com>
2021-04-09 10:30:53 -0700

    added smooth saving transition

--- scripts/menutemplate.js
+++ scripts/menutemplate.js
@@ -2,16 +2,16 @@
 // Define the function 
 // to screenshot the div
 function takeshot() {
-    let div =
-        document.getElementById('photo');
-
+    let div = document.getElementById('photo');
+    
     // Use the html2canvas
     // function to take a screenshot
     // and append it
     // to the output div
     html2canvas(div).then(
         function (canvas) {
-
+            var myobj = document.getElementById('photo')
+            myobj.remove();
             document.getElementById('output').appendChild(canvas);
 
             var imgData = canvas.toDataURL("image/jpeg", 1.0);
