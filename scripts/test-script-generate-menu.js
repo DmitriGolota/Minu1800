57fcc87e7ffd782d79bc1f0275e53d81565ff68e
Dmitri Golota <dmitri.golota@gmail.com>
2021-04-11 21:21:53 -0700

    deleted  unused files, added function for saving div to DB

--- scripts/test-script-generate-menu.js
+++ scripts/test-script-generate-menu.js
@@ -1,3 +1,18 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCV6-efUlCaZoXzuD-nnNOd200p5inM6XM",
+    authDomain: "minu1800-df37c.firebaseapp.com",
+    projectId: "minu1800-df37c",
+    storageBucket: "minu1800-df37c.appspot.com",
+    messagingSenderId: "1001139316928",
+    appId: "1:1001139316928:web:529f2df9d5341d73296525"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
+let divitem = $('#db-div').html();
+
 function writeMenus() {
     
     var menuRef = db.collection("menus");
@@ -6,12 +21,13 @@ function writeMenus() {
         name: "username",             
         picture: "somephoto.jpeg",
         template: "temp1",
-        version: 56423
+        version: 56423,
+        html: divitem.toString()
 
     });
     console.log("New collection added")
 }
-var menubtn = document.getElementById("newmenu")
+var menubtn = document.getElementById("generate_menu")
 
 menubtn.onclick = function(){
     writeMenus();
