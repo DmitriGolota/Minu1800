1a6d3466d00d4c1e4aea2c456c34f532c46ef373
Dmitri Golota <dmitri.golota@gmail.com>
2021-04-16 07:42:26 -0700

    deleted file

--- scripts/menutemplate.js
+++ scripts/menutemplate.js
@@ -13,7 +13,7 @@ takeshot = function() {
         function (canvas) {
             var myobj = document.getElementById('photo')
             var imgData = canvas.toDataURL("image/jpeg", 1.0);
-            var pdf = new jsPDF('p', 'px', [526, 564]);
+            var pdf = new jsPDF('p', 'mm', [526, 564]);
 
             pdf.addImage(imgData, 'JPEG', 0, 0);
             pdf.save("menu.pdf");
