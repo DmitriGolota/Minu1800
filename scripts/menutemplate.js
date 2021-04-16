3188e54565dc5fe69c56e5800addb30eceae923d
Heqian Liu <lhq@bu.edu>
2021-04-16 22:38:10 +0800

    fix a bug

--- scripts/menutemplate.js
+++ scripts/menutemplate.js
@@ -2,18 +2,18 @@ let pdfsave = document.getElementById('savepdf')
 
 // Define the function 
 // to screenshot the div
-takeshot = function() {
+takeshot = function () {
     let div = document.getElementById('photo');
-    
+
     // Use the html2canvas
     // function to take a screenshot
-    // and append it 
+    // and append it
     // to the output div
     html2canvas(div).then(
         function (canvas) {
             var myobj = document.getElementById('photo')
             var imgData = canvas.toDataURL("image/jpeg", 1.0);
-            var pdf = new jsPDF('p', 'px', [526, 564]);
+            var pdf = new jsPDF('p', 'mm', [525, 565]);
 
             pdf.addImage(imgData, 'JPEG', 0, 0);
             pdf.save("menu.pdf");
